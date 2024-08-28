9ddf389dbbd519e584c9c15f9d87ad49f1083622
Jeff Matheson <jeff.matheson@tetratech.com>
2024-08-28 09:22:27 -0700

    add pick random name

---
+++ 99.0 Misc/pick_random_name.R
@@ -0,0 +1,22 @@
+### Pick Random Name ###
+
+# This is some code to randomly pick a name from a list of names
+# to draw a prize. 
+
+library(tidyverse)
+library(readxl)
+
+# Create a list of names
+# names <- c("Alice", "Bob", "Charlie", "Diana", "Eve")
+
+survey <- read_excel("C:/Users/jeff.matheson/Downloads/Data Science Tools.xlsx")
+
+# Randomly pick a name from the list
+
+Winner <- survey |> 
+  filter(!is.na(Name)) |> 
+  group_by(Group) |> 
+  sample_n(1) |> 
+  select(Name)
+
+view(Winner)
